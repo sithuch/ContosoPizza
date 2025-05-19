617a9fd41c1f445c185b2ba1267b71badaf050d8
sithuch <chhemsithuch1999@gmail.com>
2025-05-19 22:19:57 +0700

    update query

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,9 +1,21 @@
 
+// namespace ContosoPizza.Models;
+
+// public class Pizza
+// {
+//     public int Id { get; set; }
+//     public string? Name { get; set; }
+//     public bool IsGlutenFree { get; set; }
+// }
+
+using System.ComponentModel.DataAnnotations;
 namespace ContosoPizza.Models;
 
 public class Pizza
 {
     public int Id { get; set; }
+    [Required]
     public string? Name { get; set; }
     public bool IsGlutenFree { get; set; }
+    
 }
